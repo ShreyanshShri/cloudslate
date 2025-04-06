fda30c7dc9cb682cb3a0f8fcf7b2b1856696834d
Shreyansh Shrivastva <shreyanshshrivastva@gmail.com>
2025-04-06 10:11:48 +0530

    Forgot to remove ai middleware in index file

--- server/http/routes/v1/index.ts
+++ server/http/routes/v1/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 
 import authRouter from "./auth/auth";
 import editorRouter from "./editor/editor";
-import aiRouter from "./ai/ai";
+// import aiRouter from "./ai/ai";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/editor", editorRouter);
-router.use("/ai", aiRouter);
+// router.use("/ai", aiRouter);
 
 export default router;
